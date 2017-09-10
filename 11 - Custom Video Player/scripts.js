0486df0ec5b7d2d240ccfe1487dbb0897ad83c6a
Jennifer Christie Christie <jennifer.christie1@gmail.com>
2017-09-09 21:38:26 -0400

    video plays

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,25 @@
+//get our elements
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('player__slider');
+
+//build our functions
+
+function togglePlay(){
+  /*const method = video.paused ? 'play' : 'pause';
+  video[method](); */  //This is works the same. It's a ternary operator.
+  if(video.paused){
+    video.play();
+  } else {
+    video.pause();
+  }
+}
+
+
+//hook up the event listeners
+video.addEventListener('click', togglePlay); //this is so the video plays and pauses when you click the video screen
+toggle.addEventListener('click', togglePlay); //this is so the video plays and pauses when you click the play button
